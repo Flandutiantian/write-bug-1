9a2e7943cce12035be99762d4def3d5537a89660
TiNa <1737438545@qq.com>
2022-08-04 11:28:36 +0800

    vue-evernote-client

---
+++ src/components/Editor.vue
@@ -0,0 +1,27 @@
+<template>
+    <div ref="editor"></div>
+</template>
+
+<script>
+    import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core';
+    import { nord } from '@milkdown/theme-nord';
+    import { commonmark } from '@milkdown/preset-commonmark';
+    export default {
+        name: 'Editor',
+        props: {
+            msg: String,
+        },
+        mounted() {
+            Editor.make()
+                .config((ctx) => {
+                    ctx.set(rootCtx, this.$refs.editor);
+                    ctx.set(defaultValueCtx, this.$props.msg);
+                })
+                .use(nord)
+                .use(commonmark)
+                .create();
+        },
+    };
+</script>
+
+<style></style>
